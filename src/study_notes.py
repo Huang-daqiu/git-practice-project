be2332eb1415b1ae6150c91127ca022acf5ed33d
hqc <664182994@qq.com>
2026-07-19 23:23:12 +0800

    modify the study_notes file

--- src/study_notes.py
+++ src/study_notes.py
@@ -13,7 +13,11 @@ def print_topics():
     for index, topic in enumerate(list_topics(), start=1):
         print(f"{index}. {topic}")
 
+def print_my_name():
+    print("My name is John Doe.")
 
 if __name__ == "__main__":
     print_topics()
+    print_my_name()
+    print("hi")
 
